5f60416b1222221df3a37fba0fcb2355278ad05c
Indubu Rahulsai <rahulsai.indubu@gmail.com>
2026-05-21 10:34:57 +0530

    Add files via upload

--- customer-churn-system/train_model.py
+++ customer-churn-system/train_model.py
@@ -87,7 +87,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 # MODEL TRAINING
 # -----------------------------
 
-model = LogisticRegression(max_iter=1000)
+model = LogisticRegression(max_iter=1000, random_state=42)
 
 model.fit(X_train, y_train)
 
